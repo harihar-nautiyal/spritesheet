a8bbe36340f7b5694cc562ced69b6529112e0bac
harihar-nautiyal <nautiyalhn@gmail.com>
2026-02-11 01:34:57 +0530

    feat: Added license

---
+++ README.md
@@ -0,0 +1,5 @@
+# Spritesheet Tool
+
+This tool is used to render animations from a spritesheet completly in browser.
+
+Powered by [H01 Team](https://h01.in) License: [](./LICENSE)
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ * Spritesheet - A Leptos web tool for rendering spritesheets.
+ * Copyright (C) 2026 Harihar Nautiyal
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 use leptos::mount::mount_to_body;
 use spritesheet::App;
 
